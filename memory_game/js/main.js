d5e902f6d9d0764de15b8a2d5b1a6df04d03d3fa
Elaine <elainel.atwork@gmail.com>
2018-11-24 15:10:13 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,31 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You have found a match!");
-}
-else { 
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You have found a match!");
+	}
+	else { 
 	alert("Sorry, try again!");
-}
+	}
+};
+
+var flipCard = function (cardId) {
+	/* var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped " + cardOne);
+		var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped " + cardTwo);
+	*/
+	console.log("User flipped " + cards[cardId]);	
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length ===2) {
+		checkForMatch();
+	};
+};
+
+
+
+flipCard(0);
+flipCard(1);
